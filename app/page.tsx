e558fd665426f6fe70930af57cbcc6766a878c53
Anderboi <Upolovnew@gmail.com>
2024-05-04 23:34:02 +0700

    feat: supabase init, projects fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/themeToggle";
+import { createClient } from "@/lib/supabase/server";
 import {
   UserButton,
   SignInButton,
@@ -7,16 +8,21 @@ import {
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
-import CreateBrief from "./create-brief/page";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+  const { data: projects } = await supabase.from("projects").select();
+
   return (
     <main className="flex min-h-screen flex-col items-center //justify-between gap-6 p-24">
       <h1>Список проектов</h1>
       <SignedIn>
         <div>
           <h3>Название проекта</h3>
+          {projects?.map((project) => (
+            <p>{project.address}</p>
+          ))}
           {/* {briefs ?  */}
           {/* <span>1</span> */}
           {/* <span>2</span> */}
